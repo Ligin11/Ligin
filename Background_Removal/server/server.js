b0e8ce828666b71c23a29b3ee398717283e9a6e5
LiginGeorge11 <ligingeorge1184@gmail.com>
2025-03-29 15:34:20 +0530

    ClientEnd

--- Background_Removal/server/server.js
+++ Background_Removal/server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
 
 app.get('/', (req,res) => res.send("API working"))

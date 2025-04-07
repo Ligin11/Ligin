910818807d89cfb6b3728a643b3ae6c72d4daff6
LiginGeorge11 <ligingeorge1184@gmail.com>
2025-04-08 01:22:05 +0530

    Correction

--- Portfolio-React/src/components/Header.jsx
+++ Portfolio-React/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
         Hi! I'm Ligin George <img src="./assets/hand-icon.png" alt="" className="w-6"/></h3>
     <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">Full stack web developer based in Chennai.</h1>
-    <p className="max-w-2xl mx-auto font-Ovo">Experienced T24 & Frontend Developer | Ex-Temenos | Open to Full Stack Roles</p>
+    <p className="max-w-2xl mx-auto font-Ovo">Experienced T24 & Frontend Developer | Open to Full Stack Roles</p>
 
     <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
 
